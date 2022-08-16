c1c7700ff842547a67d3b9b56c261853aa2dd2b4
iOS Developer <appdev@cresoty.co.kr>
2022-08-16 17:18:51 +0900

    기본 소켓통신 에코서버 적용

---
+++ modules/socket.js
@@ -0,0 +1,21 @@
+exports.initSocket = function(app) {
+  const io = app.io = require('socket.io')({
+    allowEIO3: true
+  });
+
+  io.on('connection', (socket) => {
+    const userCount = io.engine.clientsCount;
+    console.log('[total: %d] user in (%s)', userCount, socket.id);
+    socket.broadcast.emit('hi');
+
+    socket.on('disconnect', () => {
+      console.log('user out');
+    });
+
+    socket.on('chat-msg', (msg) => {
+      io.emit('chat-msg', msg);
+    });
+  
+  });
+
+}
